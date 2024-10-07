b39251c3617c82f1de6fe6b7cc2885e1a081a318
PedroV <a_pedro.vigara.haro@iespablopiccaso.es>
2024-10-07 13:06:49 +0200

    Version Final ahora si que si

--- lib/Screens/NombrePantalla.dart
+++ lib/Screens/NombrePantalla.dart
@@ -11,9 +11,28 @@ class NombrePantalla extends StatelessWidget {
       ),
       backgroundColor: Color(0xFFdaffff),
       body: Center(
-        child: Text(
-          'Pedro Vigara Haro',
-          style: GoogleFonts.lato(fontSize: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Pedro Vigara Haro',
+              style: GoogleFonts.lato(fontSize: 30),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Repositorio:',
+              style: GoogleFonts.lato(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'https://github.com/Pedro-VH05/Actividad1Multimedia',
+              style: GoogleFonts.lato(
+                fontSize: 16,
+                decoration: TextDecoration.underline,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,102 @@
-import 'package:actividad1/Screens/NombrePantalla.dart';
-import 'package:flutter/material.dart';
-import 'package:actividad1/Screens/RepoPantalla.dart';
-import 'package:actividad1/Screens/FotosPantalla.dart';
-import 'package:actividad1/Screens/FotosColumna.dart';
-import 'package:actividad1/Screens/IconosPantalla.dart';
+  import 'package:actividad1/Screens/NombrePantalla.dart';
+  import 'package:flutter/material.dart';
+  import 'package:actividad1/Screens/RepoPantalla.dart';
+  import 'package:actividad1/Screens/FotosPantalla.dart';
+  import 'package:actividad1/Screens/FotosColumna.dart';
+  import 'package:actividad1/Screens/IconosPantalla.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Drawer Navegable',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
+  void main() {
+    runApp(MyApp());
   }
-}
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Drawer Navegable'),
-        backgroundColor: Color(0xFF76e9ff),
-      ),
-      drawer: Drawer(
-        backgroundColor: Color(0xFFdaffff),
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color(0xFF76e9ff),
-              ),
-              child: Text(
-                'Menú de Navegación',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24,
-                  height: 1,
+  class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: 'Drawer Navegable',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(),
+      );
+    }
+  }
+
+  class MyHomePage extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Drawer Navegable'),
+          backgroundColor: Color(0xFF76e9ff),
+        ),
+        drawer: Drawer(
+          backgroundColor: Color(0xFFdaffff),
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color(0xFF76e9ff),
+                ),
+                child: Text(
+                  'Menú de Navegación',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                    height: 10,
+                  ),
                 ),
               ),
-            ),
-            ListTile(
-              leading: const Icon(Icons.person),
-              title: const Text('Nombre y Apellidos'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => NombrePantalla()),
-                );
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.link),
-              title: const Text('Repositorio'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RepoPantalla()),
-                );
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.photo),
-              title: const Text('Fotos en Fila'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FotosPantalla()),
-                );
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.photo_library),
-              title: const Text('Fotos en Columna'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FotosColumna()),
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.star),
-              title: Text('Iconos'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => IconosPantalla()),
-                );
-              },
-            ),
-          ],
+              ListTile(
+                leading: const Icon(Icons.person),
+                title: const Text('Nombre y Apellidos'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => NombrePantalla()),
+                  );
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.photo),
+                title: const Text('Fotos en Fila'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FotosPantalla()),
+                  );
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.photo_library),
+                title: const Text('Fotos en Columna'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FotosColumna()),
+                  );
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.star),
+                title: Text('Iconos'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => IconosPantalla()),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
-      ),
-      backgroundColor: Color(0xFFdaffff),
-      body: const Center(
-        child: Text(
-          'Selecciona una opción del menú.',
+        backgroundColor: Color(0xFFdaffff),
+        body: const Center(
+          child: Text(
+            'Selecciona una opción del menú.',
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
-}
